8ea768f14f0c8add676f2162cbe6a180e20528e5
Md Abul Bashar <hmbashar@users.noreply.github.com>
2018-01-24 14:39:18 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
+<?php
+// Product featured Image Show
 
+if ( has_post_thumbnail( get_the_ID() ) ) {
+    echo get_the_post_thumbnail( get_the_ID(), 'shop_single' );
+} else {
+    echo '<img src="' . woocommerce_placeholder_img_src() . '" alt="Placeholder" />';
+}
